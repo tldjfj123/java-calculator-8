c29218c08f3afd948c63cdefd199cd4146a31d14
tldjfj123 <dksckd0804@naver.com>
2025-10-19 19:34:55 +0900

    feat : 입력값이 null 또는 빈 문자열일 경우 0을 반환하는 기능 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringCalculator {
+    public static int add(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+        // 아직 다른 기능은 구현되지 않았습니다.
+        return 0;
+    }
+}
