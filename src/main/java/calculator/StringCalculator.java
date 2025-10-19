5d3156d0fbec0148ee2496ebd838c1fe03dc859b
tldjfj123 <dksckd0804@naver.com>
2025-10-19 19:43:41 +0900

    feat: 커스텀 구분자 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,13 +1,30 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     public static int add(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
-        String[] numbers = text.split("[,:]");
+
+        String delimiter = ",|:";
+        String numbersText = text;
+
+        Pattern pattern = Pattern.compile("//(.)\\\\\\\\n(.*)");
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            delimiter = Pattern.quote(matcher.group(1));
+            numbersText = matcher.group(2);
+        }
+
+        if (numbersText.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbers = numbersText.split(delimiter);
         return Arrays.stream(numbers)
                 .mapToInt(Integer::parseInt)
                 .sum();
