5ca9abe2e2659a23da50d2ca140277b412e27bc7
tldjfj123 <dksckd0804@naver.com>
2025-10-19 19:39:45 +0900

    feat: 기본 구분자로 분리 후 합산 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,8 +7,7 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-//        int result = StringCalculator.add(input);
-        int result = 0;
+        int result = StringCalculator.add(input);
 
         System.out.println("결과 : " + result);
     }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,11 +1,15 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public static int add(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
-        // 아직 다른 기능은 구현되지 않았습니다.
-        return 0;
+        String[] numbers = text.split("[,:]");
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
